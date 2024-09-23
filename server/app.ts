fb2ab8060e8627d14beeddeb8579af4eb385c8fc
Subham Kundu <subhamkundu.bankura12@gmail.com>
2024-09-23 07:27:55 +0530

    adding dynamic mail is complete

--- server/app.ts
+++ server/app.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 export const app = express();
 require("dotenv").config();
 import { ErrorMiddleware } from "./middleware/error";
+import userRouter from "./routes/user.route";
 
 //body parser 
 app.use(express.json({limit:"50mb"}));
@@ -16,6 +17,9 @@ app.use(cookieParser());
 app.use(cors({
     origin:process.env.ORIGIN,
 }));
+
+//routes
+app.use("/api/v1",userRouter)
 
 
 //testing api 
